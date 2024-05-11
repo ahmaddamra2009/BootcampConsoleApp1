5903c8ba65837bb6bec1e3b7c273bb5e77b9ea8c
ahmed <ahmed@DESKTOP-TJ1AJOJ>
2024-05-11 13:02:31 +0300

    Add project files.

---
+++ ConsoleApp1/Models/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    public class Employee
+    {
+
+
+        public string CalcSalary(string name,double salary,double salesAmount,int overTime)
+        {
+            double OverTimeCost =(( (salary / 30) / (8))*(overTime*1.5))+salary;
+
+            if (salesAmount>=0 && salesAmount <1000)
+            {
+                salary = salary;
+            }
+            else if (salesAmount >= 1000 && salesAmount < 5000)
+            {
+                salary = salary+(salesAmount*0.02);
+            }
+            else if (salesAmount >= 5000 && salesAmount < 10000)
+            {
+                salary = salary + (salesAmount * 0.04);
+            }
+            else if (salesAmount >= 10000 && salesAmount < 15000)
+            {
+                salary = salary + (salesAmount * 0.06);
+            }
+            else if (salesAmount >= 15000 )
+            {
+                salary = salary + (salesAmount * 0.09);
+            }
+
+            return "Name: " + name+"\n Sales Amount: " + salesAmount 
+                +"\n Total Salary : " +salary+
+                "\n Total Salary after over Time: " + OverTimeCost
+                ;
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,83 @@
+﻿
+
+using ConsoleApp1.Models;
+
+Employee employee = new
+    Employee();
+Console.WriteLine(employee.CalcSalary("Ahmad", 500, 8000, 5));
+
+
+Console.WriteLine("------ Loop Statmwnt");
+
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine("i= " + i);
+}
+
+Console.WriteLine("------ Even");
+for (int i = 2; i <= 10; i += 2)
+{
+    Console.WriteLine("i= " + i);
+}
+
+
+Console.WriteLine("------ Odd");
+for (int i = 1; i <= 10; i += 2)
+{
+    Console.WriteLine("i= " + i);
+}
+
+
+Console.WriteLine("------ New Even Numbers");
+for (int i = 1; i <= 10; i++)
+{
+    if (i % 2 == 0)
+    {
+        if (i > 4)
+        {
+            Console.WriteLine("i= " + i);
+        }
+
+    }
+
+}
+
+Console.WriteLine("------ Array");
+
+// dataType[] x= new dataType[]
+
+//int[] number=new int[10] ;
+//string msg = "";
+//for (int i = 0; i < number.Length; i++)
+//{
+//    number[i] =int.Parse( Console.ReadLine());
+//   msg=msg + number[i] + " ";
+//}
+//Console.WriteLine("------ Sum");
+
+
+//double sum = 0;
+
+
+//for (int i = 0; i < number.Length; i++)
+//{
+//    number[i] = int.Parse(Console.ReadLine());
+//    sum = sum + number[i] ;
+//}
+
+//Console.WriteLine("Total: " + sum);
+Console.WriteLine("------ Print Names");
+int x = 5;
+string[] names = new string[x] ;
+foreach (var n in names)
+{
+    Console.Write(n + ", ");
+}
+
+for (int i = 0; i < names.Length; i++)
+{
+    Console.Write(names[i] + ", ");
+}
+
+Console.ReadLine(); 
